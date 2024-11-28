6e36ea2da54d9d6185b830d1aa024e72736da930
Camila Alm <camila.salmeidatech@hotmail.com>
2024-11-28 11:03:33 -0500

    add signup route code

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,27 @@
 // /controllers/users.js
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 
-router.post('/signup', (req, res) => {
-    res.json({ message: 'Signup route is working' });
+const SALT_LENGTH = 12;
+
+router.post('/signup', async (req, res) => {
+    //res.json({ message: 'Signup route is working' });
+    try {
+        const userInDatabase = await User.findOne({ username: req.body.username });
+        if (userInDatabase) {
+            return res.status(400).json({error:'Username already taken.'})
+        }
+        const user = await User.create({                               // Create a new user with hashed password.
+            username: req.body.username,
+            hashedPassword: bcrypt.hashSync(req.body.password, SALT_LENGTH)
+        })                   
+        res.status(201).json({ user });
+    } catch(error) {
+        res.status(400).json({ error: error.message })
+    }
 });
-
 
 
 
--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 // models/user.js
 
-const mongoose = require('moongose');
+const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     username: {
@@ -13,6 +13,12 @@ const userSchema = mongoose.Schema({
         required: true,
     }
 })
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {                         // We will use `transform` to delete the field `hashedPassword` from the document. `transform` expects a callback function, and it will pass that callback function two arguments: 1- The original document from the database and 2- What is returned when the function is run.
+        delete returnedObject.hashedPassword;
+    }
+});
 
 const User = mongoose.model('User', userSchema); 
 
