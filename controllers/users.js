fd6bced22bc43903d674aee6b8987de44f20d7fb
Camila Alm <camila.salmeidatech@hotmail.com>
2024-11-28 11:54:59 -0500

    add signin route and code

--- controllers/users.js
+++ controllers/users.js
@@ -3,8 +3,9 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
-const SALT_LENGTH = 12;
+const SALT_LENGTH = 12;                       
 
 router.post('/signup', async (req, res) => {
     //res.json({ message: 'Signup route is working' });
@@ -23,8 +24,25 @@ router.post('/signup', async (req, res) => {
     }
 });
 
-
-
+router.post('/signin', async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        if (user && bcrypt.compareSync(req.body.password, user.hashedPassword)) {
+            const token = jwt.sign(
+                { username: user.username, _id: user._id },
+                process.env.JWT_SECRET
+            );
+            //res.json({ message: 'You are authorized' });
+            res.status(200).json({ token });
+            
+        } else {
+            //res.json({ message: 'Invalid credentials.' })
+            res.status(401).json({ error: 'Invalid username or password.'})
+        }
+    } catch(error) {
+        res.status(400).json({ error: error.message })
+    }
+})
 
 
 
